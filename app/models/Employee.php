c42d3dd81f022c12560506723ea756a943147e6c
Yospriadi <iyospri1999@gmail.com>
2022-06-24 17:01:12 +0700

    employee

--- app/models/Employee.php
+++ app/models/Employee.php
@@ -42,7 +42,7 @@ class Employee {
         $this->db->bind("first_name", $data['first_name']);
         $this->db->bind('last_name', $data['last_name']);
         $this->db->bind("email", $data['email']);
-        $this->db->bind("rank", $data['rank']);
+        $this->db->bind("rank", $data['ank']);
         return $this->db->execute();
     }
 
